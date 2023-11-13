14d736f20e76fd0ac6aad1b622c95b6da17fca1b
Thr6Defolt <43175754+Thr6Defolt@users.noreply.github.com>
2023-11-13 10:04:43 +0200

    Add files via upload

---
+++ Factorial processor/Factorial processor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace Factorial_processor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FactorialProcessor processor = new FactorialProcessor();
+
+            Console.WriteLine("Послідовно");
+            processor.Go(5, false);
+
+            Console.WriteLine("Паралельно");
+            processor.Go(5, true);
+
+            Console.ReadLine();
+        }
+    }
+}
